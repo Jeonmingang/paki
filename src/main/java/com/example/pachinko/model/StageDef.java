3a9e9e10e887dbbb7932898d2e328be3b50c9fe8
SAMKOREA <123339750+Jeonmingang@users.noreply.github.com>
2025-09-28 07:23:32 +0900

    Add files via upload

---
+++ src/main/java/com/example/pachinko/model/StageDef.java
@@ -0,0 +1,13 @@
+package com.example.pachinko.model;
+
+public class StageDef {
+    public int id;
+    public String name;
+    public int cup;
+    public double nextStageChance;
+    public double laneLeft;
+    public double laneCenter;
+    public double laneRight;
+    public String enterBroadcast;
+    public String upBroadcast;
+}
